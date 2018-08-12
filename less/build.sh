60b5cff689f09139474ad38992c1ddf07a7a4007
Ethan Witherington <wyddindustries@gmail.com>
2018-08-12 15:58:33 -0600

    major updates

--- less/build.sh
+++ less/build.sh
@@ -1,2 +1,9 @@
+# The Important Page
 lessc ./pages/home.less ../style.css
+# Supporting Pages
+lessc ./pages/generic.less ../eboard/style.css
+lessc ./pages/generic.less ../local/style.css
+lessc ./pages/mage.less ../mage/style.css
+lessc ./pages/meetings.less ../meetings/style.css
+lessc ./pages/generic.less ../resume/style.css
 lessc ./pages/signin.less ../signin/style.css
